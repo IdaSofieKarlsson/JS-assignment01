46a2a3f5f300121fc9a129e05702da1cb2a149c3
Ida-Sofie Karlsson <ida.sofie.karlsson@gmail.com>
2025-03-17 13:17:13 +0100

    assignment done

---
+++ arithmetic.js
@@ -0,0 +1,7 @@
+let numberOne = 10;
+let numberTwo = 4;
+console.log(`numberOne is ${numberOne} and numberTwo is ${numberTwo}.`);
+console.log(`numberOne plus numberTwo is ${numberOne + numberTwo}.`);
+console.log(`numberOne subtracted by numberTwo is ${numberOne - numberTwo}.`);
+console.log(`numberOne multiplied by numberTwo is ${numberOne * numberTwo}.`);
+console.log(`numberOne divided by numberTwo is ${numberOne / numberTwo}.`);
---
+++ variables.js
@@ -0,0 +1,4 @@
+let age = 48;
+let myName = "Ida-Sofie";
+let favoriteColor = "grey";
+console.log(`My name is ${myName}, I am ${age} years old and my favorite color is ${favoriteColor}.`);
